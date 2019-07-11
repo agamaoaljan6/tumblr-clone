42388e16815b35683a3d4a57f3787bed536f019e
Aljan Agamao <agamaoaljan@gmail.com>
2019-07-10 19:47:42 -0700

    added validations for creating posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,27 @@
 class PostsController < ApplicationController
+    
+    def index
+        @posts = Post.all.order('created_at DESC')
+    end
 
     def new
-
+        @post = Post.new
     end
 
     def create 
         @post = Post.new(post_params)
-        @post.save 
-
+        if @post.save 
         redirect_to @post
+        else
+        render :new
+        end
     end
     
     def show 
         @post = Post.find(params[:id])
     end
     
-    def index
-        @posts = Post.all.order('created_at DESC')
-    end
+  
     private
     def post_params
         params.require(:post).permit(:title, :body)
